0e5f16d119b7411abc19a350d710d28b7fb8fb29
Napster653 <100330656@alumnos.uc3m.es>
2017-12-15 21:06:38 +0100

    Comentado

--- src/heuristica/parte1.java
+++ src/heuristica/parte1.java
@@ -24,6 +24,7 @@ public class parte1
 	{
 		int n_filas, n_columnas;
 
+		// Se lee el archivo con los datos del parking
 		FileReader file = new FileReader(args[0]);
 		BufferedReader buffer = new BufferedReader(file);
 
@@ -38,6 +39,7 @@ public class parte1
 		n_columnas = Integer.parseInt(filas_columnas[1]);
 		System.out.println("Filas: " + n_filas);
 		System.out.println("Columnas: " + n_columnas);
+		// Se rellena un array con los datos leídos
 		String[][] parking = new String[n_filas][n_columnas];
 		for (int i = 0; i < n_filas; i++)
 		{
@@ -45,6 +47,7 @@ public class parte1
 			parking[i] = cadena.split(" ");
 		}
 
+		// Se imprime el array generado para comprobar que los datos son correctos
 		System.out.println(Arrays.deepToString(parking));
 		buffer.close();
 
@@ -52,6 +55,7 @@ public class parte1
 		SatWrapper satWrapper = new SatWrapper();
 		store.impose(satWrapper);
 
+		// Se declaran las variables
 		BooleanVar saleDelante [][] = new BooleanVar[n_filas][n_columnas];
 		BooleanVar saleDetras [][] = new BooleanVar[n_filas][n_columnas];
 		BooleanVar letraInferiorDelante [][] = new BooleanVar[n_filas][n_columnas];
@@ -59,11 +63,12 @@ public class parte1
 		BooleanVar letraInferiorDetras [][] = new BooleanVar[n_filas][n_columnas];
 		BooleanVar numerInferiorDetras [][] = new BooleanVar[n_filas][n_columnas];
 
+		// Se inicializan las variables necesarias
 		for (int fil = 0; fil < n_filas; fil++)
 		{
 			for (int col = 1; col < n_columnas - 1; col++)
 			{
-				if (parking[fil][col] != "__" && col != n_columnas && col != 0 && parking[fil][col-1] != "__" && parking[fil][col+1] != "__")
+				if (parking[fil][col] != "__" && parking[fil][col-1] != "__" && parking[fil][col+1] != "__")
 				{
 					saleDelante[fil][col] = new BooleanVar(store, "\n Node saleDelante");
 					saleDetras[fil][col] = new BooleanVar(store, "\n Node saleDetras");
@@ -75,13 +80,14 @@ public class parte1
 			}
 		}
 
-		int totalVariables=0;
+		int totalVariables = 0;
 
+		// Se registran todas las variables y se cuentan para saber cuántas tienen valores
 		for (int fil = 0; fil < n_filas; fil++)
 		{
 			for (int col = 1; col < n_columnas - 1; col++)
 			{
-				if (parking[fil][col] != "__" && col != n_columnas && col != 0 && parking[fil][col-1] != "__" && parking[fil][col+1] != "__")
+				if (parking[fil][col] != "__" && parking[fil][col-1] != "__" && parking[fil][col+1] != "__")
 				{
 					satWrapper.register(saleDelante[fil][col]);
 					satWrapper.register(saleDetras[fil][col]);
@@ -94,16 +100,16 @@ public class parte1
 			}
 		}
 
+		// Se crea un array en el que se almacenarán las variables sin dejar huecos
 		BooleanVar allVariables[] = new BooleanVar[totalVariables];
 
-		//Metemos las variables en un array para poder llamar al solucionador
 		int allVarIndex = 0;
 
 		for (int fil = 0; fil < n_filas; fil++)
 		{
 			for (int col = 1; col < n_columnas - 1; col++)
 			{
-				if (parking[fil][col] != "__" && col != n_columnas && col != 0 && parking[fil][col-1] != "__" && parking[fil][col+1] != "__")
+				if (parking[fil][col] != "__" && parking[fil][col-1] != "__" && parking[fil][col+1] != "__")
 				{
 					allVariables[allVarIndex] = saleDelante[fil][col];
 					allVariables[allVarIndex + 1] = saleDetras[fil][col];
@@ -116,6 +122,7 @@ public class parte1
 			}
 		}
 
+		// Se crean los arrays de literales
 		int saleDelanteLiteral[][] = new int[n_filas][n_columnas];
 		int saleDetrasLiteral[][] = new int[n_filas][n_columnas];
 		int letraInferiorDelanteLiteral[][] = new int[n_filas][n_columnas];
@@ -123,11 +130,12 @@ public class parte1
 		int numerInferiorDelanteLiteral[][] = new int[n_filas][n_columnas];
 		int numerInferiorDetrasLiteral[][] = new int[n_filas][n_columnas];
 
+		// Se inicializan los literales
 		for (int fil = 0; fil < n_filas; fil++)
 		{
 			for (int col = 1; col < n_columnas - 1; col++)
 			{
-				if (parking[fil][col] != "__" && col != n_columnas && col != 0 && parking[fil][col-1] != "__" && parking[fil][col+1] != "__")
+				if (parking[fil][col] != "__" && parking[fil][col-1] != "__" && parking[fil][col+1] != "__")
 				{
 					letraInferiorDelanteLiteral[fil][col] = satWrapper.cpVarToBoolVar(letraInferiorDelante[fil][col], 1, true);
 					letraInferiorDetrasLiteral[fil][col] = satWrapper.cpVarToBoolVar(letraInferiorDetras[fil][col], 1, true);
@@ -139,52 +147,42 @@ public class parte1
 			}
 		}
 
+		// Se añaden las cláusulas
 		for (int fil = 0; fil < n_filas; fil++)
 		{
 			for (int col = 1; col < n_columnas - 1; col++)
 			{
-				if (parking[fil][col] != "__" && col != n_columnas && col != 0 && parking[fil][col-1] != "__" && parking[fil][col+1] != "__")
+				if (parking[fil][col] != "__" && parking[fil][col-1] != "__" && parking[fil][col+1] != "__")
 				{
+					// saleDel ->letraInfDel OR numerInfDel pasado a FNC, lo mismo para Detrás
 					addClause(satWrapper, -saleDelanteLiteral[fil][col], letraInferiorDelanteLiteral[fil][col], numerInferiorDelanteLiteral[fil][col]);
 					addClause(satWrapper, -saleDetrasLiteral[fil][col], letraInferiorDetrasLiteral[fil][col], numerInferiorDetrasLiteral[fil][col]);
+					// Los coches bloqueados deben poder salir por delante OR por detrás
 					addClause(satWrapper, saleDelanteLiteral[fil][col], saleDetrasLiteral[fil][col]);
 
 					//letraInferiorDelanteLiteral
 					if(parking[fil][col+1].charAt(0) < parking[fil][col].charAt(0))
-					{
 						addClause(satWrapper, letraInferiorDelanteLiteral[fil][col]);
-					}
 					else
-					{
 						addClause(satWrapper, -letraInferiorDelanteLiteral[fil][col]);
-					}
+					
 					//letraInferiorDetrasLiteral
 					if(parking[fil][col-1].charAt(0) < parking[fil][col].charAt(0))
-					{
 						addClause(satWrapper, letraInferiorDetrasLiteral[fil][col]);
-					}
 					else
-					{
 						addClause(satWrapper, -letraInferiorDetrasLiteral[fil][col]);
-					}
+					
 					//numerInferiorDelanteLiteral
 					if((parking[fil][col+1].charAt(0) == parking[fil][col].charAt(0)) && (parking[fil][col+1].charAt(1) < parking[fil][col].charAt(1)))
-					{
 						addClause(satWrapper, numerInferiorDelanteLiteral[fil][col]);
-					}
 					else
-					{
 						addClause(satWrapper, -numerInferiorDelanteLiteral[fil][col]);
-					}
+					
 					//numerInferiorDetrasLiteral
 					if((parking[fil][col-1].charAt(0) == parking[fil][col].charAt(0)) && (parking[fil][col-1].charAt(1) < parking[fil][col].charAt(1)))
-					{
 						addClause(satWrapper, numerInferiorDetrasLiteral[fil][col]);
-					}
 					else
-					{
 						addClause(satWrapper, -numerInferiorDetrasLiteral[fil][col]);
-					}
 				}
 			}
 		}
